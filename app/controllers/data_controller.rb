632b0a75361efda721d2b3c360ecc3b6c9d5beff
monolok <antoinebertin@me.com>
2015-04-06 22:41:41 +0200

    parse

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,7 +1,11 @@
 class DataController < ApplicationController
 
 def index
-	
+	@response = RestClient.get 'https://www.betclic.fr/football/ligue-1-e4' #, {:params => {:id => 50, 'foo' => 'bar'}}	
+	@response = @response.code
 end
 
 end
+
+#RestClient.get 'http://example.com/resource', :params => {:foo => 'bar', :baz => 'qux'}
+# will GET http://example.com/resource?foo=bar&baz=qux
