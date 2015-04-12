76f1a05486739aabe2f3efc8a45f60ccd3669333
monolok <antoinebertin@me.com>
2015-04-12 22:59:56 +0200

    cron job set

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,20 +1,8 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+every 1.day, :at => '4:30 am' do
+	command "curl http://localhost:3000", :environment => 'development'
+    #runner "Datum.index", :environment => 'development'
+end
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+#whenever -w
 
-# Learn more: http://github.com/javan/whenever
+#crontab -l
