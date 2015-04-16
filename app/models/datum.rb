844f5e0b5f19e99b43d5c7072569be980be6022a
monolok <antoinebertin@me.com>
2015-04-16 15:05:59 +0200

    mailer rake

--- app/models/datum.rb
+++ app/models/datum.rb
@@ -2,22 +2,27 @@ class Datum < ActiveRecord::Base
 require 'open-uri'
 
 	def self.send_bet
-	#open("https://betsmart.herokuapp.com/test")
+
+		#get URL
 		url = "http://www.coteur.com/surebet.php"
 		doc = Nokogiri::HTML(open(url))
+		@show = doc.at_css("title").text
 
 		@game_data = Array.new
 		doc.css('tbody').each do |tr|
 			tr.css("tr").each do |f|
 				@game_data.push(f.css("td").text)
+				Datum.create(home: @game_data)
 			end
 		end
 
-		@size = @game_data.size
-		if @size >= 1
-			SureBet.sure_bet_send(@game_data, @size).deliver_now
-		end
-		
+		# @size = @game_data.size
+		# if @size >= 1
+		# 	Datum.create(home: @game_data)
+		# 	#SureBet.sure_bet_send(@game_data, @size).deliver_now
+		# else
+		# 	Datum.create(home: nil)
+		# end
 	end
 
 
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -8,7 +8,21 @@ desc "This task is called by the Heroku scheduler add-on"
 task :send_bet => :environment do
 	puts "Sending e-mail..."
 	Datum.send_bet
+	
+	if Datum.all.count >= 1
+		#Datum.create(home: @game_data)
+		@data_game = Array.new
+		Datum.all.each do |d|
+			@data_game.push(d.home)
+		end
+		SureBet.sure_bet_send(@data_game, @size).deliver_now
+		puts "done."
+		Datum.all.destroy_all
+		puts "Data deleted"
+	else
+		puts "no surebet."
+	end
 	#sh "https://betsmart.herokuapp.com/test"
-	puts "done."
+
 end
 
