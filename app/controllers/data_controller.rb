7ed0ea6eac2c04400058325d9ea531052bdec82a
monolok <antoinebertin@me.com>
2015-04-16 13:43:46 +0200

    test noko

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -12,7 +12,8 @@ def test
 	doc = Nokogiri::HTML(open(url))
 	@show = doc.at_css("title").text
 
-	if not doc.css('tfoot').text.empty? #== "Pas de surebet en ce moment"
+	#@doc_noko = doc.css('tfoot').text
+	#if @doc_noko.empty?
 		@game_data = Array.new
 		doc.css('tbody').each do |tr|
 			tr.css("tr").each do |f|
@@ -21,11 +22,13 @@ def test
 		end
 		@size = @game_data.size
 		#@test1 = true
-		SureBet.sure_bet_send(@game_data, @size).deliver_now
-	else
-		@size = 0
+		if @size >= 1
+			SureBet.sure_bet_send(@game_data, @size).deliver_now
+		end
+	#else
+	#	@size = 0
 		#@test1 = false
-	end
+	#end
 
 #s.delete("^a-zA-Z")  REMOVE ALL LETTER OR DIGIT FROM STRING ? REGEX
 
